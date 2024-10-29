1f99b3572e456c97214ea61a618c5f4a8fdae46e
João Pedro <jpteixeiraferreira@gmail.com>
2024-10-29 17:30:18 -0300

    Adicionar projetos dos exercícios do curso ao github

---
+++ part01-Part01_34.Same/src/main/java/Same.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Same {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the first string:");
+        System.out.println("Enter the second string:");
+    }
+}
